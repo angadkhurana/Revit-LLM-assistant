89b3027d8deea61d1411b8afcb579faaaa0aa47f
angadkhurana <angadkhurana28@gmail.com>
2024-05-29 21:20:24 +0100

    removed excel stuff from main file

--- Material.extension/Material_Plugin.tab/Dev.panel/GetMaterialType.pushbutton/script.py
+++ Material.extension/Material_Plugin.tab/Dev.panel/GetMaterialType.pushbutton/script.py
@@ -8,11 +8,6 @@ from Autodesk.Revit.DB import *
 
 import subprocess
 from pyrevit import  DB
-import openpyxl
-import xlwings as xw
-from pyxlsb import open_workbook
-from openpyxl import load_workbook
-from openpyxl.drawing.image import Image
 
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
@@ -44,29 +39,9 @@ if __name__ == '__main__':
     print(volume_of_materials_in_foundations)
     print(volume_of_materials_in_walls)
 
-    # Load the original Excel file
-    original_file_path = r"C:\Users\angad\OneDrive\Desktop\Walsh\original.xlsm"
-    modified_file_path = r"C:\Users\angad\OneDrive\Desktop\Walsh\original3.xlsm"
 
-    original_workbook = load_workbook(original_file_path, keep_vba=True, data_only = 'True')
 
-    # Modify specific cells
-    sheet = original_workbook['Walsh ECA Input (Gate)']  # Replace 'Sheet1' with your sheet name
-    sheet['A1'] = 'Hello'
-    sheet['B1'] = 'World'
-    sheet['A2'] = 123
-    sheet['B2'] = 456
-
-    # Copy pictures from the original workbook to the modified workbook
-    for sheet_name in original_workbook.sheetnames:
-        original_sheet = original_workbook[sheet_name]
-        modified_sheet = original_workbook[sheet_name]
-
-        for img in original_sheet._images:
-            modified_sheet.add_image(img)
-
-    # Save the modified workbook
-    original_workbook.save(modified_file_path)
+    
     
 
 
